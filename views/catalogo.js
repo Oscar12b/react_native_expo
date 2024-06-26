385eeb1f727536b7e968615faf625127bcadc68c
guapurita <guapura956@gmail.com>
2024-06-25 23:54:37 -0600

    terminado catalago y detalle

--- views/catalogo.js
+++ views/catalogo.js
@@ -4,57 +4,60 @@ import { Card, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
 const categories = [
-  { title: 'Camisa hombre', route: 'DetailScreen', price: 10.00 },
-  { title: 'Pantalón hombre', route: 'DetailScreen', price: 12.00 },
-  { title: 'Calzado masculino', route: 'DetailScreen', price: 15.00 },
-  { title: 'Camisa mujer', route: 'DetailScreen', price: 10.00 },
-  { title: 'Falda', route: 'DetailScreen', price: 10.00 },
-  { title: 'Calzado femenino', route: 'DetailScreen', price: 15.00 },
-  { title: 'Camisa parvularia', route: 'DetailScreen', price: 8.00 },
-  { title: 'Short parvularia', route: 'DetailScreen', price: 8.00 },
-  { title: 'Calzado parvularia', route: 'DetailScreen', price: 10.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 10.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 12.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 15.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 10.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 10.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 15.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 8.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 8.00 },
+    { title: 'Camisa hombre', route: 'DetailScreen', price: 10.00 },
 ];
 
 const CatalogScreen = () => {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  return (
-    <ScrollView style={styles.container}>
-      {categories.map((category, index) => (
-        <Card style={styles.card} key={index} onPress={() => navigation.navigate(category.route, { title: category.title, price: category.price })}>
-          <View style={styles.cardContent}>
-            <Image source={require('../assets/camisa.png')} style={styles.image} />
-            <Text style={styles.title}>{category.title}</Text>
-          </View>
-        </Card>
-      ))}
-    </ScrollView>
-  );
+    return (
+        <ScrollView style={styles.container}>
+            {categories.map((category, index) => (
+                <Card style={styles.card} key={index} onPress={() => navigation.navigate(category.route, { title: category.title, price: category.price })}>
+                    <View style={styles.cardContent}>
+                        <Text style={styles.title}>{category.title}</Text>
+                        <Image source={require('../assets/camisa.png')} style={styles.image} />
+                    </View>
+                </Card>
+            ))}
+        </ScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    marginBottom: 50,
-  },
-  card: {
-    marginVertical: 5,
-    borderRadius: 10,
-  },
-  cardContent: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-  },
-  image: {
-    width: 50,
-    height: 50,
-    marginRight: 10,
-  },
-  title: {
-    fontSize: 16,
-  },
+    container: {
+        flex: 1,
+        padding: 10,
+        marginBottom: 50,
+    },
+    card: {
+        marginVertical: 5,
+        borderRadius: 30,
+        backgroundColor: '#D1EDF5',
+    },
+    cardContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+    },
+    image: {
+        width: 70,
+        height: 70,
+        marginLeft: 50,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginLeft: 110
+    },
 });
 
-export default CatalogScreen;
+export default CatalogScreen;
--- views/detalles.js
+++ views/detalles.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { Card, Text, Button } from 'react-native-paper';
 
 const DetailScreen = ({ route, navigation }) => {
@@ -7,14 +7,20 @@ const DetailScreen = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Card>
-        <Card.Title title={title} />
+      <Card style={styles.card}>
+        <Text></Text>
+        <Card.Title title={title} titleStyle={styles.titulo} />
         <Card.Content>
-          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at dolor ac magna auctor.</Text>
-          <Text>Precio: ${price.toFixed(2)}</Text>
+          <Text style={styles.cuerpo}>Lorem ipsum dolor sit amet consectetur adipiscing elit quisque odio magnis auctor class, donec id phasellus vestibulum etiam aliquet molestie platea vivamus viverra. Varius nam gravida ante volutpat ornare iaculis inceptos fusce lacinia, placerat dis felis neque himenaeos massa ridiculus a quis, nullam vel vestibulum mollis sociosqu velit erat ut.</Text>
+          <Text></Text>
+          <Text></Text>
+          <Image source={require('../assets/camisa.png')} style={styles.image} />
+          <Text></Text>
+          <Text></Text>
+          <Text style={styles.precio}>Precio: ${price.toFixed(2)}</Text>
         </Card.Content>
         <Card.Actions>
-          <Button onPress={() => navigation.goBack()}>Volver</Button>
+          <Button onPress={() => navigation.goBack()} style={styles.boton} labelStyle={styles.etiquetaBoton}>Volver</Button>
         </Card.Actions>
       </Card>
     </View>
@@ -26,7 +32,48 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 16,
+    Color: '#D1EDF5'
   },
+  image: {
+    width: 100,
+    height: 100,
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  card: {
+    marginVertical: 5,
+    borderRadius: 20,
+    backgroundColor: '#D1EDF5',
+},
+precio: {
+    fontWeight: 'bold',
+    fontSize: 17
+},
+
+cuerpo: {
+    fontSize: 16,
+    textAlign: 'justify'
+},
+
+titulo: {
+    textAlign: 'center',  
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+
+},
+boton: {
+    backgroundColor: '#73B0E2', 
+    borderRadius: 10, 
+},
+
+etiquetaBoton: {
+    color: 'white',  
+  },
+
+  
+
 });
 
 export default DetailScreen;
