c85cac69d823178aaf0f0de2b9698c1984b9082c
PagetableK <pablo14gamerx3@gmail.com>
2024-07-28 23:23:24 -0600

    Carpetas y archivos

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './pantallas/login';
-import TabNavigator from './utilidades/TabNavigator';
+import TabNavigator from './navegacion/tab_navigator';
 
 const Stack = createStackNavigator();
 
