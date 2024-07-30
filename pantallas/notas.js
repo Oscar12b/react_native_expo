4b588f7fbce72ed6bb33ee4d2a925d124ac416f3
Oscar12b <106353456+PotCAR1452@users.noreply.github.com>
2024-07-30 16:41:22 -0600

    selectdropdown y funcion de fetchData

--- pantallas/notas.js
+++ pantallas/notas.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import SelectDropdown from '../componentes/select';
 
 const Notas = () => {
     return (
-        <View>
-            <Text>Notas</Text>
-        </View>
+        <SelectDropdown
+            //es casi lo mismo que un fetch data pero con un select
+            filename="services/public/trimestres.php" //nombre del archivo de la api de php pero con importe ej
+            action="readALL" //accion de la api de php
+            form={{ key: 'value' }} // Pasa el objeto de formulario si es necesario
+            busqueda={true} //si es true se puede buscar en el select y si es false no
+        />
     );
 };
 
--- utilidades/constantes.js
+++ utilidades/constantes.js
@@ -1,1 +1,15 @@
-const IP = '192.168.0.1';
+// COSTANTE DEL ENDPOINT DEL SERVIDOR
+export const SERVER_URL = 'http://192.168.137.70/api/';
+
+/**
+ * NOTA: LAS CONSTANTES SE DEFINEN DE LA SIGUIENTE MANERA
+ * (NOMBRE_SERVICIO)_API // EJEMPLO "CLIENTES_API"
+ */
+//********************************************************************************************************************-
+// ENDPOINTS DE LOS SERVICIOS 
+
+export const TRIMESTRES_API = '';
+export const ESTUDIANTES_API = '';
+export const NOTAS_API = '';
+
+
